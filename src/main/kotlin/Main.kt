3e790e817148eb9ffb3dff82209c2da6d8360fed
EnJoyXu <806056453@qq.com>
2022-06-02 00:59:32 +0800

    css加载成功，需要再对scroll的监听部分改动

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,9 @@
-import nju.lalala.demaxiya.css.UICSS
+import javafx.application.Application
+import javafx.stage.Stage
 import nju.lalala.demaxiya.view.UIView
-import tornadofx.App
-import tornadofx.launch
 import tornadofx.reloadStylesheetsOnFocus
 
 fun main(args: Array<String>) {
-    launch<MainApp>(args)
+    Application.launch(UIView::class.java,*args)
 }
 
-class MainApp : App(UIView::class, UICSS::class) {
-    init {
-        // 启用热重载CSS的功能
-        reloadStylesheetsOnFocus()
-    }
-}
--- src/main/kotlin/nju/lalala/demaxiya/view/UIView.kt
+++ src/main/kotlin/nju/lalala/demaxiya/view/UIView.kt
@@ -1,10 +1,86 @@
 package nju.lalala.demaxiya.view
-
 import javafx.application.Application
+import javafx.beans.value.ChangeListener
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.control.ScrollPane
 import javafx.scene.control.Tab
 import javafx.scene.control.TabPane
-import javafx.scene.input.MouseButton
 import javafx.scene.layout.FlowPane
-import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
-import nju.lalala.demaxiya.css.UICSS
+import java.io.File
+import kotlin.io.path.toPath
+
+
+class UIView :Application(){
+    override fun start(primaryStage: Stage) {
+
+        // 上面的Tab 板块
+        val tabHome = Tab("Home").apply {
+            content = ScrollPane().apply {
+                content = FlowPane().apply {
+
+                    children.addAll(
+                            Label("1"),
+                            Label("2"),
+                            Label("3"),
+                            Label("4"))
+                }
+
+                viewportBoundsProperty().addListener(ChangeListener { observable, oldValue, newValue ->
+                    println(observable)
+                })
+
+//                viewportBoundsProperty().addListener { _, _, bounds ->
+//                    this@apply.children.filterIsInstance<FlowPane>().apply {
+//                        prefWidth = bounds.width
+//                        prefHeight = bounds.height
+//                    }
+//                }
+            }
+        }
+
+        val tabSchool = Tab("School").apply {
+            content = ScrollPane().apply {
+                content = FlowPane().apply {
+
+                    children.addAll(
+                        Label("1"),
+                        Label("2"),
+                        Label("3"),
+                        Label("4"))
+
+                }
+            }
+        }
+
+
+        val tabpane1 = TabPane().apply { tabs.addAll(tabHome,tabSchool) }
+
+        //下面的Tab 模块
+
+
+        val root = VBox().apply {
+
+            children.add(tabpane1)
+        }
+
+
+
+        val scene = Scene(root)
+        scene.root.stylesheets.add("file:src/main/kotlin/nju/lalala/demaxiya/css/UICSS.css")
+//        scene.stylesheets.add(f.toURI().toExternalForm() )
+
+        primaryStage.apply {
+            minWidth = 570.0
+            minHeight = 400.0
+
+            this.scene = scene
+            show()
+        }
+
+
+
+    }
+}
