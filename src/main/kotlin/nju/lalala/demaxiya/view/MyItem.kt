dd761289897f9beb624dbb5e8be524a5a7296218
EnJoyXu <806056453@qq.com>
2022-06-02 16:05:02 +0800

    MyTab基本完成

--- src/main/kotlin/nju/lalala/demaxiya/view/MyItem.kt
+++ src/main/kotlin/nju/lalala/demaxiya/view/MyItem.kt
@@ -1,5 +1,34 @@
 package nju.lalala.demaxiya.view
 
+import javafx.scene.Node
+import javafx.scene.control.Label
+import javafx.scene.control.ScrollPane
+import javafx.scene.control.Tab
+import javafx.scene.layout.FlowPane
+
+
+class MyTab(string : String): Tab(string) {
+    init {
+        this.apply {
+            text = string
+            content = ScrollPane().apply {
+                content = FlowPane()
+                (this.content as FlowPane).prefHeightProperty().bind(this.heightProperty())
+                (this.content as FlowPane).prefWidthProperty().bind(this.widthProperty())
+            }
+        }
+    }
+
+    fun add(node: Node){
+        ((this.content as ScrollPane).content as FlowPane).children.add(node)
+    }
+
+    fun removeAt(index:Int){
+        ((this.content as ScrollPane).content as FlowPane).children.removeAt(index)
+    }
+
+
+}
 
 class MyLable {
 
--- src/main/kotlin/nju/lalala/demaxiya/view/UIView.kt
+++ src/main/kotlin/nju/lalala/demaxiya/view/UIView.kt
@@ -7,63 +7,63 @@ import javafx.scene.control.ScrollPane
 import javafx.scene.control.Tab
 import javafx.scene.control.TabPane
 import javafx.scene.layout.FlowPane
+import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
-import java.io.File
-import kotlin.io.path.toPath
+import tornadofx.vgrow
+import nju.lalala.demaxiya.view.MyTab
 
 
 class UIView :Application(){
     override fun start(primaryStage: Stage) {
 
         // 上面的Tab 板块
-        val tabHome = Tab("Home").apply {
-            content = ScrollPane().apply {
-                content = FlowPane().apply {
-
-                    children.addAll(
-                            Label("1"),
-                            Label("2"),
-                            Label("3"),
-                            Label("4"))
-                }
-
-                viewportBoundsProperty().addListener(ChangeListener { observable, oldValue, newValue ->
-                    println(observable)
-                })
-
-//                viewportBoundsProperty().addListener { _, _, bounds ->
-//                    this@apply.children.filterIsInstance<FlowPane>().apply {
-//                        prefWidth = bounds.width
-//                        prefHeight = bounds.height
-//                    }
-//                }
-            }
+        val tabHome = MyTab("Home").apply {
+            add(Label("1"))
+            add(Label("2"))
+            add(Label("3"))
+            add(Label("4"))
+            add(Label("5"))
         }
 
-        val tabSchool = Tab("School").apply {
-            content = ScrollPane().apply {
-                content = FlowPane().apply {
-
-                    children.addAll(
-                        Label("1"),
-                        Label("2"),
-                        Label("3"),
-                        Label("4"))
-
-                }
-            }
+        val tabSchool = MyTab("School").apply {
+            add(Label("1"))
+            add(Label("2"))
+            add(Label("3"))
+            add(Label("4"))
+            add(Label("5"))
         }
 
 
-        val tabpane1 = TabPane().apply { tabs.addAll(tabHome,tabSchool) }
+        val tabpane1 = TabPane().apply {
+            vgrow = Priority.ALWAYS
+            tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
+            tabs.addAll(tabHome,tabSchool)
+        }
 
         //下面的Tab 模块
 
+        val tabPackage = MyTab("Package").apply {
+            add(Label("1"))
+            add(Label("2"))
+            add(Label("3"))
+            add(Label("4"))
+            add(Label("5"))
+        }
 
+
+
+        val tabpane2  = TabPane().apply {
+            vgrow = Priority.ALWAYS
+            tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
+            tabs.add(tabPackage)
+        }
+
+        // root
         val root = VBox().apply {
-
+            spacing = 5.0
             children.add(tabpane1)
+            children.add(tabpane2)
         }
 
 
@@ -73,6 +73,7 @@ class UIView :Application(){
 //        scene.stylesheets.add(f.toURI().toExternalForm() )
 
         primaryStage.apply {
+            title = "LifeAssist"
             minWidth = 570.0
             minHeight = 400.0
 
